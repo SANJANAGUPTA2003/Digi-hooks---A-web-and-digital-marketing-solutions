4768c31546222069c92451b1ca601838f56fc7c5
Sanjana <poonamg0375@gmail.com>
2026-07-01 10:58:48 +0530

    logo fixed

--- src/components/LogoWordmark.tsx
+++ src/components/LogoWordmark.tsx
@@ -6,7 +6,7 @@ export function LogoWordmark({ className = '' }: LogoWordmarkProps) {
   return (
     <span className={`inline-flex max-w-[9rem] items-center overflow-hidden sm:max-w-[10.5rem] ${className}`}>
       <img
-        src="/logo.svg"
+        src="/logo.png"
         alt="DigiHooks"
         className="block h-full w-auto max-w-full object-contain object-left dark:brightness-0 dark:invert"
         draggable={false}
